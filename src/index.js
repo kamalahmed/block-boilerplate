9e966abe8543eab097ef2f512fbab39c04ed4c76
kamal <kamalacca@gmail.com>
2023-10-15 02:57:12 +0400

    update gitignore to exlcude build directory

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import { registerBlockType } from '@wordpress/blocks';
 import './style.scss';
 import './editor.scss';
 
-registerBlockType('kamal/block-boilerplate', {
-    edit() {
-        return 'Hello Editor';
-    },
-    save() {
-        return 'Hello Frontend';
-    },
-});
+registerBlockType( 'kamal/block-boilerplate', {
+	edit() {
+		return 'Hello Editor';
+	},
+	save() {
+		return 'Hello Frontend';
+	},
+} );
